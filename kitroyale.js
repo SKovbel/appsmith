d6e2b32ade89f4b3d452bf28a4b8fd4c5ff0f51b
SKovbel <s.kovbel@kitroyale.com>
2023-03-16 22:33:57 +0000

    save

--- kitroyale.js
+++ kitroyale.js
@@ -19,81 +19,68 @@
         "local": "en-GB",
         "currency": "GBP",
         "timeZone": "Europe/London",
-        "topMenu": [
-          {
+        "topMenu": {
+          "sales": {
             "title": "Sales",
             "page": "sales_sale-overview",
             "submenus": [
               {
                 "title": "Sales overview",
                 "page": "sales_sale-overview",
-                "old_page": "Dashboard"
               },
               {
                 "title": "Daily sales summary",
                 "page": "sales_daily-sales-summary",
-                "old_page": "Weekly Summary"
               },
               {
                 "title": "Ecommerce weekly",
                 "page": "sales_ecommerce-weekly",
-                "old_page": "Ecommerce Weekly"
               },
               {
                 "title": "Offline retail weekly",
                 "page": "sales_offline-retail-weekly",
-                "old_page": "Retail Weekly "
               },
               {
                 "title": "Annual sales summary",
                 "page": "sales_annual-sales-summary",
-                "old_page": "Annual Summary"
               },
               {
                 "title": "Best sellers",
                 "page": "sales_best-sellers",
-                "old_page": "Best Sellers"
               }
             ]
           },
-          {
+          "finance": {
             "title": "Finance",
             "page": "finance_finance-overview",
             "submenus": [
               {
                 "title": "Finance overview",
                 "page": "finance_finance-overview",
-                "old_page": "Finance Dashboard"
               },
               {
                 "title": "Sale summary",
                 "page": "finance_sales-summary",
-                "old_page": "Finance Overview"
               },
               {
                 "title": "Sales details",
                 "page": "finance_sales-details",
-                "old_page": "Finance Product"
               },
               {
                 "title": "Sales by payment",
                 "page": "finance_sales-by-payment",
-                "old_page": "Finance Payment"
               },
               {
                 "title": "Sales by country",
                 "page": "finance_sales-by-country",
-                "old_page": "Finance Country"
               },
               {
                 "title": "Shipping",
                 "page": "finance_shipping",
-                "old_page": "Finance Shipping"
               },
               {
                 "title": "Annual summary",
                 "page": "finance_annual-summary",
-                "old_page": "Finance Annual"
               }
             ]
           }
