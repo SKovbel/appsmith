0a57d7c4d2cb7bbe6500ef68acdfda14805fc3bc
SKovbel <s.kovbel@kitroyale.com>
2023-04-27 09:30:45 +0100

    save

--- config.js
+++ config.js
@@ -151,7 +151,7 @@
             "invoice_colulmn": "#febfdb",
             "refund_colulmn": "#bffedb",
             "positive": "#008000",
-            "negative": "#EF767A"
+            "negative": "#FF0000"
           },
           "sales_daily_sales_summary": {
               "columnTitleA": {"title": () => "Current week #" + reports.getDateA().format("WW YYYY")},
