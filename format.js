74919b1498bdade2223f0d5ee6ea78423313f95e
SKovbel <s.kovbel@kitroyale.com>
2023-05-02 08:20:46 +0100

    save

--- format.js
+++ format.js
@@ -103,7 +103,7 @@
     exports.colorizeCell = (val) => {
         if (!val) return '#F0F0F0'
         if (!(val.length > 0)) return '#F0F0F0'
-        return '#FF0000'
+        return '#FFFFFF'
     }
 
     exports.cap = (str) => {
