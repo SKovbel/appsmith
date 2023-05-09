80b6770f0d0f7c2661814575b0f32c877ed6fb95
SKovbel <s.kovbel@kitroyale.com>
2023-05-09 14:33:33 +0100

    changes

--- kitroyal.js
+++ kitroyal.js
@@ -1,22 +1,19 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    // AMD
-    define(['./config', './filter'], factory);
+      // AMD
+      define(['./config', './filter'], factory);
   } else if (typeof exports === 'object') {
-    // Node, CommonJS-like
-    module.exports = factory(require('./config'), require('./filter'));
+      // Node, CommonJS-like
+      module.exports = factory(require('./config'), require('./filter'));
   } else {
-    // Browser globals (root is window)
-    root.myModule = factory(root.config, root.filter);
+      // Browser globals (root is window)
+      root.myModule = factory(root.config, root.filter);
   }
 }(this, function (config, filter) {
   // Your module code here
   return {
-    someFunction: function () {
-      // Use functions from module1 and module2 here
-      config.config;
-      filter.filter;
-    }
+      config: config,
+      filter: filter
   };
 }));
 
