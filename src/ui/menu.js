43bad0df2cbf66adc3529027cd50b4085dd8709f
SKovbel <s.kovbel@kitroyale.com>
2023-05-15 15:54:37 +0100

    save

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -1,5 +1,6 @@
 exports.menu = {
     isActive: (submenus) => {
+        if (!submenus || submenus.length == 0) return false
         return submenus.reduce((x, item) => {
             return appsmith.URL.pathname.includes(item.page.replaceAll('_', '-')) 
                 ? true
