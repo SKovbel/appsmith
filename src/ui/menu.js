5f4a46642061ea2d64785837e2d43ab7259b43cf
SKovbel <s.kovbel@kitroyale.com>
2023-05-16 12:24:55 +0100

    save

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -6,52 +6,50 @@ exports.menu = {
         "topActive": '#FFFFFF',
     },
 
-    "submenu": {
-        "sales": {
-            "title": "Sales",
-            "icon": "timeline-bar-chart",
-            "submenus": [
-                {"title": "Sales overview", "page": "sales_sale-overview"},
-                {"title": "Daily sales summary", "page": "sales_daily-sales-summary"},
-                {"title": "Ecommerce weekly", "page": "sales_ecommerce-weekly"},
-                {"title": "Offline retail weekly", "page": "sales_offline-retail-weekly"},
-                {"title": "Annual sales summary", "page": "sales_annual-sales-summary"},
-                {"title": "Best sellers", "page": "sales_best-sellers"}
-            ]
-        },
+    "sales": {
+        "title": "Sales",
+        "icon": "timeline-bar-chart",
+        "submenus": [
+            {"title": "Sales overview", "page": "sales_sale-overview"},
+            {"title": "Daily sales summary", "page": "sales_daily-sales-summary"},
+            {"title": "Ecommerce weekly", "page": "sales_ecommerce-weekly"},
+            {"title": "Offline retail weekly", "page": "sales_offline-retail-weekly"},
+            {"title": "Annual sales summary", "page": "sales_annual-sales-summary"},
+            {"title": "Best sellers", "page": "sales_best-sellers"}
+        ]
+    },
 
-        "finance": {
-            "title": "Finance",
-            "icon": "timeline-area-chart",
-            "submenus": [
-                {"title": "Finance overview", "page": "finance_finance-overview"},
-                {"title": "Sale summary", "page": "finance_sales-summary"},
-                {"title": "Product detail", "page": "finance_poduct-details"},
-                {"title": "Sales by payment", "page": "finance_sales-by-payment"},
-                {"title": "Sales by country", "page": "finance_sales-by-country"},
-                {"title": "Shipping", "page": "finance_shipping"},
-                {"title": "Annual summary", "page": "finance_annual-summary"},
-                {"title": "Coupon details", "page": "finance_coupon"}
-            ]
-        },
+    "finance": {
+        "title": "Finance",
+        "icon": "timeline-area-chart",
+        "submenus": [
+            {"title": "Finance overview", "page": "finance_finance-overview"},
+            {"title": "Sale summary", "page": "finance_sales-summary"},
+            {"title": "Product detail", "page": "finance_poduct-details"},
+            {"title": "Sales by payment", "page": "finance_sales-by-payment"},
+            {"title": "Sales by country", "page": "finance_sales-by-country"},
+            {"title": "Shipping", "page": "finance_shipping"},
+            {"title": "Annual summary", "page": "finance_annual-summary"},
+            {"title": "Coupon details", "page": "finance_coupon"}
+        ]
+    },
 
-        "product": {
-            "title": "Product",
-            "icon": "timshopping-cart",
-            "disabled": true,
-            "submenus": [
-                {"title": "Page 1", "page": "",  "visible": false}
-            ]
-        },
+    "product": {
+        "title": "Product",
+        "icon": "timshopping-cart",
+        "disabled": true,
+        "submenus": [
+            {"title": "Page 1", "page": "",  "visible": false}
+        ]
+    },
 
-        "merchandizing": {
-            "title": "Merchandizing",
-            "icon": "flash",
-            "disabled": true,
-            "submenus": [
-                {"title": "Page 1", "page": "",  "visible": false}
-            ]
-        }
+    "merchandizing": {
+        "title": "Merchandizing",
+        "icon": "flash",
+        "disabled": true,
+        "submenus": [
+            {"title": "Page 1", "page": "",  "visible": false}
+        ]
     },
 
     isActive: (submenus) => {
