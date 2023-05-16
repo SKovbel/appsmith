44bc9722edcf4cb4c25b64f979166242be65247e
SKovbel <s.kovbel@kitroyale.com>
2023-05-16 12:21:48 +0100

    save

--- src/config/dict.js
+++ src/config/dict.js
@@ -17,4 +17,22 @@ exports.dict = {
         "stripe": "Stripe",
         "braintree_paypal": "Paypal braintree"
     },
+
+
+    "page": {
+        "sales_sale-overview": "Sales overview",
+        "sales_daily-sales-summary": "Daily sales summary",
+        "sales_ecommerce-weekly": "Ecommerce weekly",
+        "sales_offline-retail-weekly": "Offline retail weekly",
+        "sales_annual-sales-summary": "Annual sales summary",
+        "sales_best-sellers": "Best sellers",
+        "finance_finance-overview": "Finance overview",
+        "finance_sales-summary": "Sale summary",
+        "finance_poduct-details": "Product detail",
+        "finance_sales-by-payment": "Sales by payment",
+        "finance_sales-by-country": "Sales by country",
+        "finance_shipping": "Shipping",
+        "finance_annual-summary": "Annual summary",
+        "finance_coupon": "Coupon details"
+    }
 };
--- src/ui/style.js
+++ src/ui/style.js
@@ -1,12 +1,25 @@
-exports.uiStyle = {
-    colorizeValue: (val, negativeOnly) => {
+exports.style = {
+    "color": {
+        "positive": "#008000",
+        "negative": "#FF0000",
+
+        "set": [
+            "#1896c6",
+            "#EF767A",
+            "#EEB868",
+            "#49DCB1",
+            "#456990",
+        ],
+    },
+
+    valueColor: (val, negativeOnly) => {
         const v = format.toInt(val)
         if (v == 0) return ''
         if (v > 0 && negativeOnly) return ''
         return v > 0 ? ui.colors.positive  : ui.colors.negative
     },
 
-    colorizeCell: (val) => {
+    valueBackground: (val) => {
         if (!val) return '#F0F0F0'
         if (!(val.length > 0)) return '#F0F0F0'
         return '#FFFFFF'
